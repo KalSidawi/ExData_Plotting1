eea6f7e10e8b58d8500821471d1756ad0b9f6b3e
KalSidawi <kal.sidawi@gmail.com>
2022-11-11 23:47:43 -0700

    plot3.R successfully generates plot3.png

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
-## this script plot2.R produces the plot2.png in the root directory
-## which is the xy plot showing Global Active Power in Kilowatts vs time
+## this script plot3.R produces the plot2.png in the root directory
+## which is the xy plot showing energy sub-metering 1, 2 & 3 vs time
 ## the statistical data of power consumption is present in the file 
 ## "household_power_consumption.txt"
 ## and is filtered to the target range of Feb 1 and 2 of 2007
@@ -9,13 +9,8 @@ library(tibble)
 library(tidyverse)
 rm(list = ls())
 setwd("C:/code/Rprog/exploration/ExData_Plotting1")
-varclasses <- c("character", "character", rep("numeric",7) )
-if(file.exists("household_power_consumption.txt"))
+if(!file.exists("household_power_consumption.txt"))
    {
-     consumption <- read.table("household_power_consumption.txt", 
-                header = TRUE, sep = ";", na.strings = "?", dec = ".",
-                colClasses = varclasses)
-   } else {
     stop("file \"household_power_consumption.txt\" does not exist!")
   }
 # read the data set
@@ -23,17 +18,24 @@ classes <- c(rep(x = "character",2),rep(x = "numeric", 7))
 dataset <- as.data.table(read.table(file = "household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", colClasses = classes))
 # subsetting to get data for Feb 1 and Feb 2, 2007
 sub <- subset(x = dataset, Date == "1/2/2007" | Date == "2/2/2007" )
+remove(dataset)
 # concatenating Date and Time fields and then converting to POSIXlt Date/Time format field
 DateTime <- strptime(paste(sub$Date, sub$Time, sep = " "), format = "%d/%m/%Y %H:%M:%S")
 # adding the new DateTime field to the Data set in column 1
 sub <- data.table(DateTime, sub)
-
 ###############################
 # Data is now ready to be plotted
 ###############################
-with(sub, plot(y = Global_active_power, x = DateTime, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+with(sub, plot(y = Sub_metering_1, x = DateTime, type = "l", col = "black", ylim = c(0, 30), xlab = "", ylab = "Energy sub metering"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), fill = c("black", "red", "blue"))
+par("new" = TRUE)
+with(sub, plot(y = Sub_metering_2, x = DateTime, type = "l", col = "red", ylim = c(0, 30), ann = FALSE, axes = FALSE))
+par("new" = TRUE)
+with(sub, plot(y = Sub_metering_3, x = DateTime, type = "l", col = "blue", ylim = c(0, 30), ann = FALSE, axes = FALSE))
+
 ## copy the plot to a png file
-dev.copy(png, file = "plot2.png")
+dev.copy(png, file = "plot3.png")
 dev.off()
 dev.off()
 
